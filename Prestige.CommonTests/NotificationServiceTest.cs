8df41616e8095ada5c90916a928d7f6933b5c640
poiwert <max080405@ukr.net>
2023-08-28 13:28:58 +0300

    Added Prestige.CommonTests project and renamed class file to NotificationServiceTest

---
+++ Prestige.CommonTests/NotificationServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Prestige.CommonTests
+{
+    [TestClass]
+    public class NotificationServiceTest
+    {
+        [TestMethod]
+        public void TestNotifyTalent()
+        {
+        }
+    }
+}
---
+++ Prestige.CommonTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prestige.CommonTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Prestige.CommonTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7dff1a5d-a1b8-48ff-9574-8afeedb75bfd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
