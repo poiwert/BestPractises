e260c0ca46180685139fdcf86e851ef240d015d4
poiwert <max080405@ukr.net>
2023-08-28 12:53:36 +0300

    Added constructor Actor(string actorName) that ActorName = actorName, and Actor() that print "An actor is born."

--- Prestige.Biz/Actor.cs
+++ Prestige.Biz/Actor.cs
@@ -8,6 +8,25 @@ namespace Prestige.Biz
 {
     public class Actor
     {
+        public Actor()
+        {
+            Console.WriteLine("An actor is born.");
+        }
+
+        public Actor(string actorName):this()
+        {
+            ActorName = actorName;
+        }
+
+        private string actorName;
+
+        public string ActorName
+        {
+            get { return actorName; }
+            set { actorName = value; }
+        }
+
+
         private string jobTitle;
 
         public string JobTitle
