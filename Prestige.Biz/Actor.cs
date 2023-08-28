6408929ae088ebfdfc84c333567205ead358d2a0
poiwert <max080405@ukr.net>
2023-08-28 12:47:39 +0300

    Added property JobTitle

--- Prestige.Biz/Actor.cs
+++ Prestige.Biz/Actor.cs
@@ -8,8 +8,22 @@ namespace Prestige.Biz
 {
     public class Actor
     {
+        private string jobTitle;
+
+        public string JobTitle
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+
+
+        /// <summary>
+        /// Will return title
+        /// </summary>
+        /// <returns></returns>
         public string GetOccupation() {
-            return "Actor";
+            jobTitle = "Actor";
+            return jobTitle;
         }
     }
 }
